d107d0b14129ada7b86080bb0d41dcabff56e27c
ashleyer <ashleye.romano@gmail.com>
2021-09-21 00:26:02 -0400

    begin to code, many errors, not DRY

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # projectZero--MASH
+
+
+The late-80's future-telling game played with pen and paper meets the 21st century.
---
+++ scripts/app.js
@@ -0,0 +1,26 @@
+
+//many many many mistakes
+
+arr1 = [HTMLFormElement.input1, HTMLFormElement.input2, HTMLFormElement.input3]
+//or
+arr1[]= HTMLFormElement[i]
+//or
+arr1 = HTMLFormElement[]
+????
+
+var selectForm = document.forms[index]
+
+
+const petBox = {
+    var petA = prompt("pet choice1", "<petA>");
+    var petB = prompt("pet choice2", "<petB>");
+    var petC = prompt("pet choice3", "<petC>");
+    
+    if(prompt.input) = null {
+        console.warning('enter pet choice');
+    } else{
+        document.petA.innerHTML = valueof(petA)
+
+    }
+}
+
