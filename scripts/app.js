375222dce7af4ba5878b71dbc965d079a392b7a3
ashleyer <ashleye.romano@gmail.com>
2021-09-22 13:25:36 -0400

    draft of about 60% MVP

--- scripts/app.js
+++ scripts/app.js
@@ -1,26 +1,115 @@
 
-//many many many mistakes
+//1 global variables
+//2 functions
+//3 event listeners
 
-arr1 = [HTMLFormElement.input1, HTMLFormElement.input2, HTMLFormElement.input3]
-//or
-arr1[]= HTMLFormElement[i]
-//or
-arr1 = HTMLFormElement[]
-????
+console.log('it is working')
 
-var selectForm = document.forms[index]
+function playMASH() {
+    alert("Are you ready? Click PLAY");
+};
+
+let job1 = prompt("type in a job");
+console.log("this is box 1 typed" + job1);
+let jobArray = [];
+jobArray.push(job1);
+console.log(jobArray);
+
+let job2 = prompt('type in another job');
+console.log('this is box 2 typed' + job2);
+jobArray.push(job2);
+console.log(jobArray);
+
+let job3 = prompt('type in third job');
+console.log('this is box 3 typed' + job3);
+jobArray.push(job3);
+console.log(jobArray);
 
 
-const petBox = {
-    var petA = prompt("pet choice1", "<petA>");
-    var petB = prompt("pet choice2", "<petB>");
-    var petC = prompt("pet choice3", "<petC>");
-    
-    if(prompt.input) = null {
-        console.warning('enter pet choice');
-    } else{
-        document.petA.innerHTML = valueof(petA)
+let jList = document.getElementById("jobList");
+jobArray.forEach((i) => {
+    let li = document.createElement("li");
+    li.innerText = i;
+    jList.appendChild(li);
+});
 
-    }
-}
 
+let job1 = prompt("type in a job");
+console.log("this is box 1 typed" + job1);
+let jobArray = [];
+jobArray.push(job1);
+console.log(jobArray);
+
+let job2 = prompt('type in another job');
+console.log('this is box 2 typed' + job2);
+jobArray.push(job2);
+console.log(jobArray);
+
+let job3 = prompt('type in third job');
+console.log('this is box 3 typed' + job3);
+jobArray.push(job3);
+console.log(jobArray);
+
+
+let jList = document.getElementById("jobList");
+jobArray.forEach((i) => {
+    let li = document.createElement("li");
+    li.innerText = i;
+    jList.appendChild(li);
+});
+
+
+let job1 = prompt("type in a job");
+console.log("this is box 1 typed" + job1);
+let jobArray = [];
+jobArray.push(job1);
+console.log(jobArray);
+
+let job2 = prompt('type in another job');
+console.log('this is box 2 typed' + job2);
+jobArray.push(job2);
+console.log(jobArray);
+
+let job3 = prompt('type in third job');
+console.log('this is box 3 typed' + job3);
+jobArray.push(job3);
+console.log(jobArray);
+
+
+let jList = document.getElementById("jobList");
+jobArray.forEach((i) => {
+    let li = document.createElement("li");
+    li.innerText = i;
+    jList.appendChild(li);
+});
+
+
+let job1 = prompt("type in a job");
+console.log("this is box 1 typed" + job1);
+let jobArray = [];
+jobArray.push(job1);
+console.log(jobArray);
+
+let job2 = prompt('type in another job');
+console.log('this is box 2 typed' + job2);
+jobArray.push(job2);
+console.log(jobArray);
+
+let job3 = prompt('type in third job');
+console.log('this is box 3 typed' + job3);
+jobArray.push(job3);
+console.log(jobArray);
+
+
+let jList = document.getElementById("jobList");
+jobArray.forEach((i) => {
+    let li = document.createElement("li");
+    li.innerText = i;
+    jList.appendChild(li);
+});
+
+
+
+
+let randomJob = jobArray[Math.floor(Math.random() * jobArray.length)];
+console.log(randomJob);
